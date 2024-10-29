89a09a041a5f7325282a658af9c91d9475afe3ea
AndyTomickiFind <andy.tomicki@find.co>
2024-10-29 16:58:36 +0100

    Added full menu locators.

--- tests/fixtures/fixtures.ts
+++ tests/fixtures/fixtures.ts
@@ -1,17 +1,17 @@
 import { test as base } from '@playwright/test';
 import {HomePage} from "../page-objects/home.page";
-import {HeaderComponent} from "../page-objects/components/headerComponent";
+import {MenuComponent} from "../page-objects/components/menuComponent";
 
 
 
 export const test = base.extend<{
     HomePage: HomePage,
-    headerComponent: HeaderComponent,
+    headerComponent: MenuComponent,
 }>({
     HomePage: async ({ page, context }, use, testInfo) => {
         await use(new HomePage(page, context, testInfo));
     },
     headerComponent: async ({ page, context }, use, testInfo) => {
-        await use(new HeaderComponent(page, context, testInfo));
+        await use(new MenuComponent(page, context, testInfo));
     },
 });
