025631415d2024be474b20bf50c460d4a6823593
AndyTomickiFind <andy.tomicki@find.co>
2024-11-07 16:50:32 +0100

    Add test for Multi Grid verification
    Added a new test to check the Multi Grid functionality on the Short Reviews Page. This test ensures that critical fields in a selected row contain the expected values, verifying the grid's accuracy.

--- tests/sanity/shortReviewsPage.spec.ts
+++ tests/sanity/shortReviewsPage.spec.ts
@@ -15,7 +15,7 @@ test.describe(`SHORT REVIEWS subpage - ${config.name} `, {tag: [`@${config.name}
 
     test('verify that the expected menu items are displayed and clickable', async ({menuComponent}) => {
         await menuComponent.checkMenuItems()
-       // await menuComponent.checkDataPointItems() //doesnt exist in staging
+        // await menuComponent.checkDataPointItems() //doesnt exist in staging
     });
 
     test('check the alert banner', async ({components}) => {
@@ -47,5 +47,18 @@ test.describe(`SHORT REVIEWS subpage - ${config.name} `, {tag: [`@${config.name}
             ]
             await components.checkCombobox(ShortReviewsPage.toplistProductCombobox, toplistComboboxItems);
         });
+    });
+
+    test('check the Multi Grid', async ({components, ShortReviewsPage}) => {
+        await components.clickItemFromCombobox(ShortReviewsPage.toplistCombobox, "QA Toplist");
+
+        await test.step("`Check each of the fields in a row", async () => {
+            await expect(components.dataGridCell("toplistName", 1)).toContainText("QA Toplist");
+            await expect(components.dataGridCell("partnerName", 1)).toContainText("Dunder Casino");
+            await expect(components.dataGridCell("status", 1)).toContainText("Todo");
+            await expect(components.dataGridCell("assignedTo", 1)).toContainText("");
+            await expect(components.dataGridCell("updatedAt", 1)).toContainText("11/7/2024,");
+            await expect(components.dataGridCell("actions", 1)).toHaveText("Approve");
+        })
     });
 });
