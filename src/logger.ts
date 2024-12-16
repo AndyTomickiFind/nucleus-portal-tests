657512506b3c1a48db440b33f4908caf09cc871b
AndyTomickiFind <andy.tomicki@find.co>
2024-12-16 12:36:43 +0100

    https://findco.atlassian.net/browse/DEV-5372
    Add test for `language` parameter in toplist API endpoint
    
    Introduced a Playwright test to validate API responses for different `language` query parameters. Includes expected responses for staging, ensuring functionality aligns with specifications, especially for edge cases (e.g., unsupported languages).

--- src/logger.ts
+++ src/logger.ts
@@ -2,7 +2,7 @@ import {APIResponse, TestInfo} from "@playwright/test";
 
 export async function logResponse(response: APIResponse, context: TestInfo): Promise<void> {
     const responseBody = await response.body();
-    await context.attach('Response Body', {
+    await context.attach(`Response Body of ${response.url()}`, {
         contentType: 'application/json',
         body: responseBody.toString()
     });
---
+++ tests/api/language_param.spec.ts
@@ -0,0 +1,45 @@
+import {expect, test} from '@playwright/test';
+import {logResponse} from '../../src/logger';
+import config from "../../playwright.config";
+import fs from "fs";
+
+test(`[${config.name.toUpperCase()}] GET /api/v2/toplists/{id}/results`, {
+    annotation: {
+        type: 'issue',
+        description: 'https://findco.atlassian.net/browse/DEV-5372',
+    },
+}, async ({request}, testInfo) => {
+
+    const languages = [
+        'en',
+        'fr',
+        'es',
+        'pt',
+        'pl',  //should return errorcode 400
+    ];
+
+    for (const language of languages) {
+        await test.step(`Checking language: ${language}`, async () => {
+            const testData = JSON.parse(fs.readFileSync(`tests/api/testData_${process.env.TEST_ENV}/cryptogambling-toplistsIds.json`, 'utf-8'));
+            const response = await request.get(`https://${config.toplistServiceV1Uri}/api/v2/toplists/${testData._id}/results`, {
+                headers: {
+                    // Add headers if needed
+                },
+                params: {
+                    'language': `${language}`
+                },
+                data: {
+                    // Add body data if needed
+                }
+            });
+
+            await logResponse(response, testInfo);
+
+            // expect(response.status()).toBe(200);
+            const fullExpectedResponse = JSON.parse(fs.readFileSync(`tests/api/expectedResponses_${process.env.TEST_ENV}/language_params/language_${language}.json`, 'utf-8'));
+            // Parse the response body as JSON
+            const responseBody = await response.json();
+            expect(responseBody).toMatchObject(fullExpectedResponse);
+        });
+    }
+});
