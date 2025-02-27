ed1145a8a990c2d101a15402b8adc2f8049bff39
AndyTomickiFind <andy.tomicki@find.co>
2025-02-27 13:05:44 +0100

    Refactor clickItemFromCombobox to set exact parameter default.
    Updated the `exact` parameter in `clickItemFromCombobox` to have a default value of `true`. This improves clarity and reduces the need for explicitly passing the parameter in most cases. Removed commented-out code for better readability.

--- tests/page-objects/components/components.ts
+++ tests/page-objects/components/components.ts
@@ -81,13 +81,13 @@ export class components extends BasePage {
     }
 
 
-    async clickItemFromCombobox(comboboxLocator: Locator, item: string, exact?:boolean): Promise<void> {
+    async clickItemFromCombobox(comboboxLocator: Locator, item: string, exact: boolean = true
+    ): Promise<void> {
         await comboboxLocator.click();
         await comboboxLocator.locator("input").fill(item);
         await this.page.waitForLoadState();
         const itemLocator: Locator = this.page.getByRole('option', {name: item, exact: exact})
         await itemLocator.click();
-      //  await this.page.keyboard.press('Escape');
     }
 
     async selectFromDropdown(comboboxLocator: Locator, item: string): Promise<void> {
