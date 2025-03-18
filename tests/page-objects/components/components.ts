7ee702db13864694eddaed7af49f80569e59269b
AndyTomickiFind <andy.tomicki@find.co>
2025-03-18 14:03:54 +0100

    Refactor country list to a centralized reusable constant.
    Moved duplicated country lists to a single reusable constant in `components.ts`. This removes redundancy and simplifies maintenance by ensuring a unified source of truth for the list of countries. Also adjusted a timeout value in `exchanges.page.ts` for improved performance.

--- tests/page-objects/components/components.ts
+++ tests/page-objects/components/components.ts
@@ -24,6 +24,53 @@ export class components extends BasePage {
     readonly chipButton: (label: string) => Locator;
     readonly chipButtonCloseX: (label: string) => Locator;
 
+    readonly countriesList: string[] = [
+        "Angola", "Antarctica", "Argentina", "Aruba", "Australia", "Bangladesh",
+        "Belarus", "Belize", "Bosnia and Herzegovina", "Botswana", "Bouvet Island",
+        "British Indian Ocean Territory", "Cambodia", "Canada", "China", "Comoros",
+        "Congo, the Democratic Republic of the", "Cuba", "Curaçao", "Czech Republic",
+        "Djibouti", "Ecuador", "Ethiopia", "Fiji", "Gambia", "Georgia", "Gibraltar",
+        "Greece", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan", "Lesotho", "Malawi",
+        "Mali", "Malta", "Marshall Islands", "Mayotte", "Monaco", "Morocco", "Myanmar",
+        "Nepal", "Netherlands Antilles", "Niger", "Norfolk Island", "Paraguay", "Peru",
+        "Puerto Rico", "Saint Helena, Ascension and Tristan da Cunha", "Saint Martin (French part)",
+        "Sierra Leone", "South Georgia and the South Sandwich Islands", "South Korea", "Sudan",
+        "Suriname", "Sweden", "Syrian Arab Republic", "Tanzania, United Republic of", "Thailand",
+        "Tonga", "Turks and Caicos Islands", "Uganda", "United Arab Emirates", "United States Minor Outlying Islands",
+        "Uzbekistan", "Vietnam", "Western Sahara", "Zimbabwe", "Afghanistan", "Algeria",
+        "American Samoa", "Armenia", "Barbados", "Bermuda", "Bonaire, Sint Eustatius and Saba",
+        "Burkina Faso", "Cayman Islands", "Chad", "Chile", "Egypt", "Equatorial Guinea",
+        "Eritrea", "Falkland Islands (Malvinas)", "Faroe Islands", "French Southern Territories",
+        "Gabon", "Ghana", "Grenada", "Guernsey", "Guinea-Bissau", "Guyana", "Haiti", "Hungary",
+        "Indonesia", "Iran, Islamic Republic of", "Iraq", "Jamaica", "Jersey", "Jordan",
+        "Lao People's Democratic Republic", "Liberia", "Lithuania", "Macedonia, the former Yugoslav Republic of",
+        "Maldives", "Mauritania", "Mexico", "Micronesia, Federated States of", "Mozambique",
+        "Netherlands", "New Caledonia", "Niue", "Norway", "Pakistan", "Palau", "Poland", "Russia",
+        "Saint Barthélemy", "Saint Kitts and Nevis", "Samoa", "Senegal", "Slovakia", "Slovenia", "Somalia",
+        "South Sudan", "Sri Lanka", "Switzerland", "Trinidad and Tobago", "Tunisia", "Turkey",
+        "Ukraine", "United Kingdom", "United States", "Uruguay", "Vanuatu", "Anguilla",
+        "Antigua and Barbuda", "Bolivia", "Brunei", "Bulgaria", "Cameroon", "Colombia",
+        "Congo", "Denmark", "Dominican Republic", "Estonia", "Finland", "French Guiana",
+        "French Polynesia", "Guadeloupe", "Holy See (Vatican City State)", "Hong Kong",
+        "Ireland", "Italy", "Lebanon", "Libya", "Madagascar", "Moldova, Republic of",
+        "Montenegro", "Montserrat", "Nauru", "Nigeria", "Oman", "Panama", "Papua New Guinea",
+        "Saint Lucia", "Saint Pierre and Miquelon", "San Marino", "Sao Tome and Principe",
+        "Saudi Arabia", "Serbia", "Seychelles", "South Africa", "Spain", "Taiwan", "Tajikistan",
+        "Togo", "Tokelau", "Virgin Islands, U.S.", "Wallis and Futuna", "Yemen", "Zambia",
+        "Åland Islands", "Albania", "Andorra", "Austria", "Azerbaijan", "Bahamas", "Bahrain",
+        "Belgium", "Benin", "Bhutan", "Brazil", "Burundi", "Cape Verde", "Central African Republic",
+        "Christmas Island", "Cocos (Keeling) Islands", "Cook Islands", "Costa Rica", "Croatia",
+        "Cyprus", "Dominica", "El Salvador", "France", "Germany", "Greenland", "Guam", "Guatemala",
+        "Guinea", "Heard Island and McDonald Islands", "Honduras", "Iceland", "India", "Isle of Man",
+        "Israel", "Ivory Coast", "Japan", "Kazakhstan", "Kenya", "Korea, Democratic People's Republic of",
+        "Latvia", "Liechtenstein", "Luxembourg", "Macao", "Malaysia", "Martinique", "Mauritius",
+        "Mongolia", "Namibia", "New Zealand", "Nicaragua", "Northern Mariana Islands",
+        "Palestinian Territory, Occupied", "Philippines", "Pitcairn", "Portugal", "Qatar", "Réunion",
+        "Romania", "Rwanda", "Saint Vincent & the Grenadines", "Singapore", "Sint Maarten (Dutch part)",
+        "Solomon Islands", "Svalbard and Jan Mayen", "Swaziland", "Timor-Leste", "Turkmenistan", "Tuvalu",
+        "Venezuela", "Virgin Islands, British"
+    ];
+
     constructor(page: Page, context: BrowserContext, testInfo: TestInfo) {
         super(page, context);
         this.page = page;
