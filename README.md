6b8b22c9afed5c547c14cff6934ea525e969efd2
AndyTomickiFind <AndyTomickiFind@users.noreply.github.com>
2025-04-10 10:21:23 +0000

    deploy: 3ea1e54e201256a3977aecb0ef081c8bed46510c

--- README.md
+++
@@ -1,5 +0,0 @@
-# nucleus-portal-tests
-
-This is just a POC sketch, do not expect anything fancy.
-
-Any questions - andy.tomicki@find.co
--- src/logger.ts
+++
@@ -1,9 +0,0 @@
-import {APIResponse, TestInfo} from "@playwright/test";
-
-export async function logResponse(response: APIResponse, context: TestInfo, method?: string): Promise<void> {
-    const responseBody = await response.body();
-    await context.attach(`Response Body of ${method} ${response.url()} Status: ${response.status()}`, {
-        contentType: 'application/json',
-        body: responseBody.toString()
-    });
-}
